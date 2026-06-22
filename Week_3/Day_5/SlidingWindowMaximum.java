77ca630c518a4616ad1263763a35078cde910d09
KhyatiPrakash <khyatiprakash2005@gmail.com>
2026-06-22 22:07:52 +0530

    Implement sliding window maximum algorithm

--- Week_3/Day_5/SlidingWindowMaximum.java
+++ Week_3/Day_5/SlidingWindowMaximum.java
@@ -1,1 +1,38 @@
+import java.util.*;
 
+class Solution {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove elements outside window
+            while (!deque.isEmpty() &&
+                   deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+
+            // Remove smaller elements
+            while (!deque.isEmpty() &&
+                   nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            // Store answer
+            if (i >= k - 1) {
+                result[idx++] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
